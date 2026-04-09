251c3d36039a3d66003e6bed54af2151b39118e3
Sta4ik <sta4ik@tutamail.com>
2026-04-09 17:26:53 +0300

    all task complete

--- Lab3.sql
+++ Lab3.sql
@@ -227,7 +227,9 @@ GROUP BY FACULTY.id, FACULTY.faculty_name
 
 /* Задание SELECT 11 */
 
-SELECT SUBJ.id, SUBJ.subj, MAX(SUBJ.hours) FROM SUBJ
+SELECT SUBJ.id, SUBJ.subj, MAX(HOURS.all_h) FROM SUBJ
+JOIN WORK ON SUBJ.id = WORK.subj_id
+JOIN HOURS ON WORK.hours_id = HOURS.id
 GROUP BY SUBJ.id, SUBJ.subj
 
 /* Задание SELECT 12 */
@@ -290,14 +292,98 @@ WHERE FACULTY.id = ( SELECT TOP 1 FACULTY.id
                      GROUP BY FACULTY.id
                      ORDER BY COUNT(STUD.id) DESC )
 
-/* Задание подзапрос 8 */
+/* Задание подзапрос 4 */
 
-SELECT STUD.f_name, STUD.last_name, STUD.s_name, (SELECT  ) FROM PROCESS
-JOIN STUD ON PROCESS.stud_id = STUD.id
+SELECT DISTINCT STUD.id, STUD.s_name, STUD.f_name, STUD.last_name, FACULTY.faculty_name, HOURS.course, FORM.form_name FROM STUD
+JOIN PROCESS ON STUD.id = PROCESS.stud_id
 JOIN HOURS ON PROCESS.hours_id = HOURS.id
 JOIN FACULTY ON HOURS.faculty_id = FACULTY.id
 JOIN FORM ON HOURS.form_id = FORM.id
+WHERE NOT EXISTS (
+	SELECT 1 FROM PROCESS P2
+	JOIN HOURS H2 ON P2.hours_id = H2.id
+	JOIN STUD S2 ON P2.stud_id = S2.id
+	WHERE H2.faculty_id = HOURS.faculty_id
+	AND H2.course = HOURS.course
+	AND H2.form_id = FORM.id
+	AND (S2.last_name IS NULL OR S2.last_name = '')
+)
+
+/* Задание подзапрос 5 */
+
+SELECT DISTINCT STUD.id, STUD.s_name, STUD.f_name, STUD.last_name, HOURS.course
+FROM STUD
+JOIN PROCESS ON STUD.id = PROCESS.stud_id
+JOIN HOURS ON PROCESS.hours_id = HOURS.id
+WHERE HOURS.course = (
+	SELECT DISTINCT H2.course
+	FROM STUD S2
+	JOIN PROCESS P2 ON S2.id = P2.stud_id
+	JOIN HOURS H2 ON P2.hours_id = H2.id
+	WHERE S2.s_name = 'Ботяновский'
+) AND STUD.s_name != 'Ботяновский'
+
+/* Задание подзапрос 6 */
+
+SELECT DISTINCT STUD.id, STUD.s_name, STUD.f_name, STUD.last_name
+FROM STUD
+JOIN PROCESS ON STUD.id = PROCESS.stud_id
+JOIN HOURS ON PROCESS.hours_id = HOURS.id
+WHERE HOURS.course IN (
+	SELECT H2.course
+	FROM STUD S2
+	JOIN PROCESS P2 ON S2.id = P2.stud_id
+	JOIN HOURS H2 ON P2.hours_id = H2.id
+	WHERE S2.s_name = 'Зингель'
+		INTERSECT
+	SELECT H3.course
+	FROM STUD S3
+	JOIN PROCESS P3 ON S3.id = P3.stud_id
+	JOIN HOURS H3 ON P3.hours_id = H3.id
+	WHERE S3.s_name = 'Зайцева' )
+AND STUD.s_name NOT IN ('Зингель', 'Зайцева')
+
+/* Задание подзапрос 7 */
+
+SELECT DISTINCT STUD.id, STUD.s_name, STUD.f_name, STUD.last_name, FACULTY.faculty_name, HOURS.course, FORM.form_name FROM STUD
+JOIN PROCESS ON STUD.id = PROCESS.stud_id
+JOIN HOURS ON PROCESS.hours_id = HOURS.id
+JOIN FACULTY ON HOURS.faculty_id = FACULTY.id
+JOIN FORM ON HOURS.form_id = FORM.id
+WHERE (
+	SELECT COUNT(DISTINCT S2.id)
+	FROM PROCESS P2
+	JOIN HOURS H2 ON P2.hours_id = H2.id
+	JOIN STUD S2 ON P2.stud_id = S2.id
+	WHERE H2.faculty_id = HOURS.faculty_id
+	AND H2.course = HOURS.course
+	AND H2.form_id = FORM.id
+	AND (S2.last_name IS NULL OR S2.last_name = '')) > 1
+
+/* Задание подзапрос 8 */
+
+SELECT DISTINCT STUD.id, STUD.s_name, STUD.f_name, STUD.last_name, FACULTY.faculty_name,HOURS.course, ( SELECT COUNT(DISTINCT S2.id) FROM PROCESS P2
+																										JOIN HOURS H2 ON P2.hours_id = H2.id
+																										JOIN STUD S2 ON P2.stud_id = S2.id
+																										WHERE H2.faculty_id = HOURS.faculty_id
+																										AND H2.course = HOURS.course) FROM STUD
+JOIN PROCESS ON STUD.id = PROCESS.stud_id
+JOIN HOURS ON PROCESS.hours_id = HOURS.id
+JOIN FACULTY ON HOURS.faculty_id = FACULTY.id
 WHERE STUD.last_name IS NULL OR STUD.last_name = ''
+
+/* Задание UNION 1 */
+
+SELECT TEACH.id, TEACH.s_name, TEACH.f_name, TEACH.last_name, SUM(SUBJ.hours),
+       CASE 
+           WHEN SUM(SUBJ.hours) > 450 THEN '20%'
+           WHEN SUM(SUBJ.hours) > 300 THEN '10%'
+           ELSE '0%'
+       END 
+FROM TEACH
+JOIN WORK ON TEACH.id = WORK.teach_id
+JOIN SUBJ ON WORK.subj_id = SUBJ.id
+GROUP BY TEACH.id, TEACH.s_name, TEACH.f_name, TEACH.last_name
 
 /* Задание UNION 2 */
 
@@ -418,6 +504,7 @@ BEGIN
 	JOIN HOURS ON FACULTY.id = HOURS.faculty_id
 	JOIN PROCESS ON HOURS.id = PROCESS.hours_id
 	JOIN STUD ON PROCESS.stud_id = STUD.id
+	JOIN FORM ON HOURS.form_id = FORM.id
 	WHERE FORM.form_name = @form_name AND FACULTY.faculty_name = @faculty_name
 	GROUP BY FACULTY.id, FACULTY.faculty_name, FORM.form_name
 END
@@ -425,3 +512,35 @@ GO
 
 /* Задание по процедурам 2 */
 
+CREATE PROCEDURE CountSubject
+AS
+BEGIN
+    DECLARE @fpkCount INT, @fpmCount INT, @totalCount INT, @sameCount INT
+	
+	SELECT @fpkCount = COUNT(DISTINCT SUBJ.id) FROM SUBJ
+	JOIN WORK ON WORK.subj_id = SUBJ.id
+	JOIN HOURS ON HOURS.id = WORK.hours_id
+	JOIN FACULTY ON FACULTY.id = HOURS.faculty_id
+	WHERE FACULTY.faculty_name = 'ФПК'
+
+	SELECT @fpmCount = COUNT(DISTINCT SUBJ.id) FROM SUBJ
+	JOIN WORK ON WORK.subj_id = SUBJ.id
+	JOIN HOURS ON HOURS.id = WORK.hours_id
+	JOIN FACULTY ON FACULTY.id = HOURS.faculty_id
+	WHERE FACULTY.faculty_name = 'ФПМ'
+
+	SELECT @totalCount = COUNT(DISTINCT SUBJ.id) FROM SUBJ
+	JOIN WORK ON WORK.subj_id = SUBJ.id
+	JOIN HOURS ON HOURS.id = WORK.hours_id
+
+	SELECT @sameCount = COUNT(DISTINCT s1.id) FROM SUBJ s1
+	WHERE EXISTS (SELECT 1 FROM WORK w1
+                  JOIN HOURS h1 ON h1.id = w1.hours_id
+                  JOIN FACULTY f1 ON f1.id = h1.faculty_id
+                  WHERE w1.subj_id = s1.id AND f1.faculty_name = 'ФПК')
+      AND EXISTS (SELECT 1 FROM WORK w2
+                  JOIN HOURS h2 ON h2.id = w2.hours_id
+                  JOIN FACULTY f2 ON f2.id = h2.faculty_id
+                  WHERE w2.subj_id = s1.id AND f2.faculty_name = 'ФПМ')
+    PRINT('Для ФПК читается ' + CONVERT(NVARCHAR, @fpkCount) + ' предметов, для ФПМ читается ' + CONVERT(NVARCHAR, @fpmCount) + ' предметов, всего ' + CONVERT(NVARCHAR, @totalCount) + ' предметов (' + CONVERT(NVARCHAR, @sameCount) + ' из которых идентичны)')
+END
